3e79e83f0270270bb1eecf4261607e4ade34fc68
Wéko <42733086+wekotsai@users.noreply.github.com>
2018-12-13 22:33:26 +0100

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,6 @@
 <template>
  <div class="test">
  <h1>2018 World Cup Highlights</h1>
-  <div class="video-responsive">
-   <iframe width="560" height="315" src="https://www.youtube.com/embed/Vw34wMAqWzc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-  </div> 
  </div> 
 
 </template>
